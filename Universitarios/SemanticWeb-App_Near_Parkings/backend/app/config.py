2613fb4908dc39a4b49edd7e16454f32001c9790
SergioZSZ <95578449+SergioZSZ@users.noreply.github.com>
2025-11-22 18:10:52 +0100

    c

--- Universitarios/SemanticWeb-App_Near_Parkings/backend/app/config.py
+++ Universitarios/SemanticWeb-App_Near_Parkings/backend/app/config.py
@@ -1,11 +1,14 @@
+import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+#en local pilla las de mi .env y en fly las de .env SECRETO que genera, todo bien mientras no suba
+#mi .env a github, que entonces si usara ese
 class Settings(BaseSettings):
-    APP_NAME: str
+    APP_NAME: str = "Near_Parkings"
     DATABASE_URL: str
 
     
-# Indicamos a Pydantic que lea el archivo .env
+
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
     
 # Instancia global para usar las variables entorno de .env
