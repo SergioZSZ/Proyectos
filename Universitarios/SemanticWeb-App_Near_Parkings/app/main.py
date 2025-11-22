925b34bc4e51051f4e04ff5f697d46ccefa39aeb
SergioZSZ <95578449+SergioZSZ@users.noreply.github.com>
2025-11-22 14:04:49 +0100

    añadido requirements para elbackend unicamente y requirements_frontend para la app unicamente

--- Universitarios/SemanticWeb-App_Near_Parkings/app/main.py
+++ Universitarios/SemanticWeb-App_Near_Parkings/app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI, APIRouter
 from fastapi.staticfiles import StaticFiles
+import uvicorn
 from app.routers import parkings, favoritos
-
+import os
 app = FastAPI()
 
 app.include_router(parkings.router, prefix="/parkings", tags=["parkings"])
 app.include_router(favoritos.router, prefix="/favoritos", tags=["favoritos"])
+
+
+@app.get("/")
+def root():
+    return {"status": "Near_Parkings API OK"}
+
+
+#uvicorn app.main:app --host 0.0.0.0 --port $PORT
+
+
