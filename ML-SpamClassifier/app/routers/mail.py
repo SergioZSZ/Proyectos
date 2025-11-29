a2cf48f6433eb21dcffaf557ec38a0c9400eca85
SergioZSZ <s.zaballosh@gmail.com>
2025-11-29 17:45:39 +0100

    Update mail.py

--- ML-SpamClassifier/app/routers/mail.py
+++ ML-SpamClassifier/app/routers/mail.py
@@ -2,16 +2,36 @@ from fastapi import APIRouter, HTTPException, status
 from app import schemas
 import joblib
 from app.schemas.mail import Tipo
+import tensorflow as tf
 #from model.funciones_auxiliares import clean_text, create_model
 router = APIRouter()
-#model = joblib.load("model/models/modelTrained.joblib")
-model = joblib.load("model/models/modelSVC.joblib")
-@router.post("/predict",response_model=schemas.MailOutput, status_code=status.HTTP_200_OK)
+modelsvc = joblib.load("model/models/modelSVC.joblib")
+model = tf.keras.models.load_model("model/models/modelMLP.keras")
+@router.post("/predictMLP",response_model=schemas.MailOutput, status_code=status.HTTP_200_OK)
 async def predict(mensaje_input: schemas.MailInput):
     tipo_pred: str
+    x = tf.constant([mensaje_input.message])
     
+    pred = model.predict(x)
     
-    pred = model.predict([mensaje_input.message])
+    if pred[0] < 0.5:
+        tipo_pred = Tipo.ham
+    else:
+        tipo_pred = Tipo.spam
+
+    mensaje_output = schemas.MailOutput(
+        message=mensaje_input.message,
+        tipo=tipo_pred
+    )
+        
+    return mensaje_output
+
+
+@router.post("/predictSVC",response_model=schemas.MailOutput, status_code=status.HTTP_200_OK)
+async def predict(mensaje_input: schemas.MailInput):
+    tipo_pred: str
+    pred = modelsvc.predict([mensaje_input.message])
+    
     if pred[0] == 0:
         tipo_pred = Tipo.ham
     else:
